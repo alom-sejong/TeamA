2fe78fd19ff6eb03bf29833b1653000710cf8769
이승규 <seung4465@icloud.com>
2019-05-20 00:24:15 +0900

    category

--- app/src/main/java/com/alomateam/gainapp/Pay/CategoryActivity.java
+++ app/src/main/java/com/alomateam/gainapp/Pay/CategoryActivity.java
@@ -18,12 +18,12 @@ public class CategoryActivity extends AppCompatActivity {
 
         ImageView category[] = new ImageView[8];
         for(int i = 0; i < 8; i++){
-            category[i] = (ImageView)findViewById(R.id.ca_1 + i);
+            category[i] = findViewById(R.id.ca_1 + i);
             final int k = i;
             category[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(getApplicationContext(),  Integer.toString(k)+ " clicked", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),  k+ " clicked", Toast.LENGTH_SHORT).show();
                 }
             });
         }
