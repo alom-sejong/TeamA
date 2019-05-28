25fb99229bb92a6a81fe2f32eeeda61d95df7349
이승규 <seung4465@icloud.com>
2019-05-29 01:24:16 +0900

    category

--- app/src/main/java/com/alomateam/gainapp/Pay/CategoryActivity.java
+++ app/src/main/java/com/alomateam/gainapp/Pay/CategoryActivity.java
@@ -1,5 +1,7 @@
 package com.alomateam.gainapp.Pay;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +10,34 @@ import android.widget.Toast;
 
 import com.alomateam.gainapp.R;
 
+import java.util.ArrayList;
+
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+
 public class CategoryActivity extends AppCompatActivity {
 
+    AutoScrollViewPager autoViewPager;
+    ImageView category[] = new ImageView[8];
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        ArrayList<String> data = new ArrayList<>(); //이미지 url를 저장하는 arraylist
+        data.add("https://upload.wikimedia.org/wikipedia/en/thumb/2/24/SpongeBob_SquarePants_logo.svg/1200px-SpongeBob_SquarePants_logo.svg.png");
+        data.add("http://nick.mtvnimages.com/nick/promos-thumbs/videos/spongebob-squarepants/rainbow-meme-video/spongebob-rainbow-meme-video-16x9.jpg?quality=0.60");
+        data.add("http://nick.mtvnimages.com/nick/video/images/nick/sb-053-16x9.jpg?maxdimension=&quality=0.60");
+        data.add("https://www.gannett-cdn.com/-mm-/60f7e37cc9fdd931c890c156949aafce3b65fd8c/c=243-0-1437-898&r=x408&c=540x405/local/-/media/2017/03/14/USATODAY/USATODAY/636250854246773757-XXX-IMG-WTW-SPONGEBOB01-0105-1-1-NC9J38E8.JPG");
 
-        ImageView category[] = new ImageView[8];
+        autoViewPager = (AutoScrollViewPager)findViewById(R.id.autoViewPager);
+        AutoScrollAdapter scrollAdapter = new AutoScrollAdapter(this, data);
+        autoViewPager.setAdapter(scrollAdapter); //Auto Viewpager에 Adapter 장착
+        autoViewPager.setInterval(5000); // 페이지 넘어갈 시간 간격 설정
+        autoViewPager.startAutoScroll(); //Auto Scroll 시작
+
+
         for(int i = 0; i < 8; i++){
             category[i] = findViewById(R.id.ca_1 + i);
             final int k = i;
@@ -27,6 +48,5 @@ public class CategoryActivity extends AppCompatActivity {
                 }
             });
         }
-
     }
 }
